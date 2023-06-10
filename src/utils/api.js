8366287626e7a7dca30b19238499b237f4769bd2
techseason <amanchaudharykumar@gmail.com>
2023-06-10 23:20:00 +0530

    initial commit

---
+++ src/utils/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+        // cff92b9afdmsh6b9399888d909a9p17b2a9jsna213fff71cae
+            process.env.REACT_APP_YOUTUBE_API_KEY,
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
